67cbea3c56f84a63d0864c3d04058baddd71df90
Bruno Bulnes <bulnes.bruno@gmail.com>
2020-08-07 00:23:39 -0300

    Gerenciamento de videos com React e JavaScript

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-import Home from './pages/Home'
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/Categoria'
-
-const Pagina404 = () => (<div>Página 404 (colocar um jogo)</div>)
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
-      <Route path="/" component={Home} exact />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
